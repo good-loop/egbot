bc02c743f584f7d76c3f3b4d6a362fc95a508fe6
Irina Preda <irinalaurapreda@gmail.com>
2018-06-18 18:19:49 +0100

    wrote up pre-processing and feature extraction for dedoose data

---
+++ extractDedoose.py
@@ -0,0 +1,131 @@
+###################################################################################
+## Description:                                                                 ###
+## Status: WIP															        ###
+###################################################################################
+
+from nltk.corpus import stopwords
+from wordcloud import WordCloud
+from sklearn.feature_extraction import DictVectorizer
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import operator
+import sklearn
+
+#========== reading data 
+df = pd.read_csv('data/dedoose.csv', encoding='utf-8')
+
+egbotdo = dict()
+egbotdont = dict()
+
+docount = 0
+dontcount = 0
+
+#========== constructing frequency dictionaries
+for index, row in df.iterrows():
+    words = row['Excerpt Copy'].lower().split(' ')
+    label = row['Meta Label']
+    
+    if label in 'egbotdont':
+        dontcount += 1
+    if label in 'egbotdo':
+        docount += 1 
+
+    for word in words:
+        if label in 'egbotdont':
+            if word in egbotdont:
+                egbotdont[word] += 1
+            else:
+                egbotdont[word] = 1
+        if label in 'egbotdo':
+            if word in egbotdo:
+                egbotdo[word] += 1
+            else:
+                egbotdo[word] = 1
+
+# sort them based on frequency count, descending
+freq_do = sorted(egbotdo.items(), key=operator.itemgetter(1), reverse=True)
+freq_dont = sorted(egbotdont.items(), key=operator.itemgetter(1), reverse=True)
+
+#=========== word cloud
+
+# remove stopwords => nltk 
+stop = set(stopwords.words('english'))
+
+egbotdo_filtered = dict()
+egbotdont_filtered = dict()
+
+for word in egbotdo:
+    if word not in stop:
+        egbotdo_filtered[word] = egbotdo[word]
+for word in egbotdont:
+    if word not in stop:
+        egbotdont_filtered[word] = egbotdont[word]
+
+# generate a word cloud image for egbotdo
+wordcloud = WordCloud().generate_from_frequencies(egbotdo_filtered, max_font_size=40)
+
+plt.title('egbotdo')
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')
+
+# generate a word cloud image for egbotdont
+wordcloud = WordCloud().generate_from_frequencies(egbotdont_filtered, max_font_size=40)
+
+plt.figure()
+plt.title('egbotdont')
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis('off')
+#plt.show()
+
+# sort them based on frequency count, descending
+freq_do = sorted(egbotdo_filtered.items(), key=operator.itemgetter(1), reverse=True)
+freq_dont = sorted(egbotdont_filtered.items(), key=operator.itemgetter(1), reverse=True)
+
+#========== feature selection
+
+# bag of words model - term frequency
+matrix = []
+matrix.append(egbotdo)
+matrix.append(egbotdont)
+v = DictVectorizer(sparse=False)
+X = v.fit_transform(matrix)
+#print v.get_feature_names()
+
+
+#unique = list(set(freq_do+freq_dont))
+#bowdo = [0]*len(unique)
+##bowdont = [0]*len(unique)
+
+#print unique
+# for word in egbotdont:
+#     print word
+#     bowdont[unique.index(word)] = egbotdont[word]
+#     break
+        # if label in 'egbotdo':
+        #     bowdo[unique.index(word)] += 1
+
+
+# later: phrase based features, part of speech features, semantic features, social cues, etc?
+# named entity recog: rule-based, generative models - HMM, conditional models - MeMM or CRF, deep learning - RNN?
+
+#========== clustering 
+
+#========== rule-based
+
+#========== classification 
+# w/ and w/o stemming
+# try knn: bayes, svm, decision trees, random forests 
+
+#========== testing
+
+#=========== general improvements
+# should i use tf-idf to normalise terms?
+# n gram model?
+# needs more data => input some mse data as well
+
+
+
+
+
+
