8967ccbf825445c01b2d74de5b7c8c7e68131461
Irina Preda <irinalaurapreda@gmail.com>
2018-06-21 14:08:40 +0100

    added vectorisation script

--- data-collection/2_vectoriseBuildData.py
+++ data-collection/2_vectoriseBuildData.py
@@ -1,15 +1,16 @@
 ###################################################################################
-## Description: Process raw dedoose data into build data                        ###
+## Description: Vectorise build data                                            ###
 ## Status: WIP															        ###
 ###################################################################################
-
-import sys, time
+from nltk.corpus import stopwords
+from sklearn import feature_extraction, preprocessing
+import sys, time, re, string
 import pandas as pd
 
-# script settings
-iteration = 0 																				## UPDATE THIS: to improve data management
+## script settings
+iteration = 1 													## UPDATE THIS: to improve data management
 
-# path to data folder
+## path to data folder
 path = '/home/irina/data'
 
 if len(sys.argv) > 1:
@@ -25,10 +26,32 @@ else:
     dataset = path + '/dedoose.csv'
     cols = ['Excerpt','Label'] 
 
-# read data
+## read data
 df = pd.read_csv(dataset, usecols=cols, encoding='utf-8')
 
-# exporting dataset to csv
+stop = set(stopwords.words('english')).union([''])
+
+## Vectorise using counts
+f = feature_extraction.text.CountVectorizer(preprocessor, stop_words='english', analyzer='word', token_pattern=u'(?ui)\\b\\w*[a-z]+\\w*\\b', binary=True) # automatically generates vocabulary 
+X = f.fit_transform(df[cols[0]]) # generates counts
+ 
+# print f.get_feature_names() # the list of features (unique words)
+# print X.toarray() # the matrix showing the counts 
+
+## Apply tf-idf term weighting
+#f = feature_extraction.text.TfidfTransformer(smooth_idf=False)
+#tfidf = f.fit_transform(X.toarray()) # generates weights of each feature
+
+## Binarize
+#X = preprocessing.binarize(X) # no need for counts, just a binary value representing existence of the word in the excerpt
+#y = df['Label'].map({'egbotdont':1,'egbotdo':0}) # binarize labels as well
+
+
+newX = pd.DataFrame(data=X.toarray().transpose(), index=f.get_feature_names())
+vectorisedDF = newX.transpose().assign(**df.drop(['Excerpt'], axis=1)).copy()
+
+
+## exporting dataset to csv
 try:
-	df.to_csv(path + '/2_vectoriseBuildData_%d_%d.csv' % (iteration, int(time.time())), sep=',', encoding='utf-8')
+    vectorisedDF.to_csv(path + '/2_vectoriseBuildData_%d_%d.csv' % (iteration, int(time.time())), sep=',', encoding='utf-8')
 except UnicodeEncodeError as e: print(e)
