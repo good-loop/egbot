d7778a65f53da3d23d60082a18d78db284950cf9
Irina Preda <irinalaurapreda@gmail.com>
2018-06-25 12:34:52 +0100

    adding base files for skeleton

---
+++ src/js/app.jsx
@@ -0,0 +1,10 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MainDiv from './components/MainDiv';
+
+ReactDOM.render(
+	<MainDiv />,
+	document.getElementById('mainDiv')
+);
---
+++ src/js/components/MainDiv.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class MainDiv extends Component {
+	render() {
+
+		return (
+		      <div className="App">
+		      </div>
+		    );
+	} // ./render()
+} // ./MainDiv
+
+export default MainDiv;
+
+
