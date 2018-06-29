024ae21cf3ede6a03cbb351dae8064c35adb3c71
Irina Preda <irinalaurapreda@gmail.com>
2018-06-29 11:29:54 +0100

    added header

--- src/js/components/MainDiv.jsx
+++ src/js/components/MainDiv.jsx
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import Misc from '../base/components/Misc';
 
 class MainDiv extends Component {
-	render() {
 
+
+
+	componentWillMount() {
+	}
+
+	render() {
 		return (
-		      <div className="App">
-		      </div>
-		    );
+			<div>
+				<div className="container avoid-navbar">
+					<div className="page MyPage">
+						<Misc.Card>
+							<div className="header">
+								<div className="header-text">
+									<p className="title">EgBot</p>
+									<p className="subtitle">A question & answer site<br/> for your math questions.</p>
+								</div>
+							</div>
+						</Misc.Card>
+					</div>
+				</div>
+			</div>
+		);
 	} // ./render()
 } // ./MainDiv
 
 export default MainDiv;
-
-
