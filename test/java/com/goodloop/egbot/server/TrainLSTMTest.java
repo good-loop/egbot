ae7536fa88dbc62d0c95d1a8469b0f82685d6810
Irina Preda <irinalaurapreda@gmail.com>
2018-11-16 18:24:10 +0000

    writing test for loading certain vocab and training with it

--- test/java/com/goodloop/egbot/server/TrainLSTMTest.java
+++ test/java/com/goodloop/egbot/server/TrainLSTMTest.java
@@ -22,9 +22,8 @@ public class TrainLSTMTest {
 	public void testTrain() throws IOException {
 		int ckptVersion = new Random().nextInt(1000000);
 		TrainLSTM lstm = new TrainLSTM(ckptVersion);
-
-		int vocabVersion = 957780;
-		lstm.loadVocab(vocabVersion);
+		lstm.loadAndInitVocab();
+		lstm.loadVocab(ckptVersion);
 		lstm.loadAndTrain();
 	}
 	
