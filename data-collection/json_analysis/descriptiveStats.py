c845dd3950933636f283923c529c457b9c4d1884
Irina Preda <irinalaurapreda@gmail.com>
2018-12-06 13:10:24 +0000

    added closed post as well as no accepted answer analysis

--- data-collection/json_analysis/descriptiveStats.py
+++ data-collection/json_analysis/descriptiveStats.py
@@ -14,8 +14,21 @@ avgScorePerAccA = 0
 noOfQsWithPosScore = 0
 noOfQsWithGoodScore = 0
 
+noOfAsWithPosScore = 0
+noOfAsWithGoodScore = 0
+
+noOfQsWithGoodScoreANoAccA = 0
+noOfAsWithPosScoreNotAcc = 0
+noOfAsWithGoodScoreNotAcc = 0
+
 lenOfQs = 0
 lenOfAs = 0
+
+noOfClosed = 0
+noOfClosedDuplicates = 0
+noOfClosedOffTopic = 0
+noOfClosedOpinion = 0
+noOfClosedUnclear = 0
 
 for no in range(1,9):
     filename = "MathStackExchangeAPI_Part_" + str(no) + ".json"
@@ -31,23 +44,54 @@ for no in range(1,9):
             lenOfQs += len(data[i]["body_markdown"])
             score = data[i]["score"]
             avgScorePerQ += score
+
             if (score >= 0):
                 noOfQsWithPosScore += 1
             if (score > 0):
                 noOfQsWithGoodScore += 1
+
+            if("closed_details" in data[i].keys()):
+                noOfClosed += 1
+                reason = data[i]["closed_details"]["reason"]
+                if "duplicate" in reason:  noOfClosedDuplicates += 1;
+                elif "off-topic" in reason:  noOfClosedOffTopic += 1;
+                elif "opinion" in reason:  noOfClosedOpinion += 1;
+                elif "unclear" in reason:  noOfClosedUnclear += 1;
+
+            hasPosA = False
+            hasGoodA = False
+            hasAccA = False
+
             if("answers" in data[i].keys()):
                 noOfQsWithA += 1
                 for j in range(0, len(data[i]["answers"])):
                     noOfAs += 1
                     ans = dict()
                     ans["body_markdown"] = data[i]["answers"][j]["body_markdown"]
-                    ans["is_accepted"] = data[i]["answers"][j]["is_accepted"]
+                    lenOfAs += len(ans["body_markdown"])
+
                     ans["score"] = data[i]["answers"][j]["score"]
                     avgScorePerA += ans["score"]
-                    lenOfAs += len(ans["body_markdown"])
+                    if (ans["score"] >= 0):
+                        noOfAsWithPosScore += 1
+                        hasPosA = True
+                    if (ans["score"] > 0):
+                        noOfAsWithGoodScore += 1
+                        hasGoodA = True
+
+                    ans["is_accepted"] = data[i]["answers"][j]["is_accepted"]
                     if(ans["is_accepted"]):
                         noOfQsWithAccA += 1
                         avgScorePerAccA += ans["score"]
+                        hasAccA = True
+                    else:
+                        if (ans["score"] >= 0):
+                            noOfAsWithPosScoreNotAcc += 1
+                        if (ans["score"] > 0):
+                            noOfAsWithGoodScoreNotAcc += 1
+            if (not hasAccA):
+                if (hasGoodA):
+                    noOfQsWithGoodScoreANoAccA += 1
 print()            
 print("Total no of questions: ", noOfQs)
 print("Total no of answers: ", noOfAs)
@@ -61,14 +105,28 @@ print()
 print("Average question score: ", avgScorePerQ/noOfQs)
 print("No of questions with a positive score: ", noOfQsWithPosScore)
 print("No of questions with a good score: ", noOfQsWithGoodScore)
+
+print()
 print("Average answer score: ", avgScorePerA/noOfAs)
 print("Average accepted answer score: ", avgScorePerAccA/noOfQsWithAccA)
-print()      
+print("No of answers with a positive score: ", noOfAsWithPosScore)
+print("No of answers with a good score: ", noOfAsWithGoodScore)
 
+print()
+print("No of questions with a good score answer but no accepted answer: ", noOfQsWithGoodScoreANoAccA)
+print("No of answers with a positive score but not accepted: ", noOfAsWithPosScoreNotAcc)
+print("No of answers with a good score but not accepted: ", noOfAsWithGoodScoreNotAcc)
 
 print()            
 print("Average question length: ", lenOfQs/noOfQs)
 print("Average answer length: ", lenOfAs/noOfAs)
-print()          
 
+print()            
+print("Number of closed questions: ", noOfClosed)
+print("Number of duplicate questions: ", noOfClosedDuplicates)
+print("Number of off-topic questions: ", noOfClosedOffTopic)
+print("Number of opinion-based questions: ", noOfClosedOpinion)
+print("Number of unclear questions: ", noOfClosedUnclear)
+
+print()      
 print("Done :)")
