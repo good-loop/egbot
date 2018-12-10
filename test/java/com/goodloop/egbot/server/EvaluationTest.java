4cb666bdd4beb51a7905a6470092c1989f38ca7b
Irina Preda <irinalaurapreda@gmail.com>
2018-12-10 15:35:22 +0000

    test eval

---
+++ test/java/com/goodloop/egbot/server/EvaluationTest.java
@@ -0,0 +1,14 @@
+package com.goodloop.egbot.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EvaluationTest {
+
+	@Test
+	public void test() throws Exception {	
+		EvaluatePredictions.run();
+	}
+
+}
