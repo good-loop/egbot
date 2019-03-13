4e2a11cd53abbde9637ce64910fef5f3e9d96810
Irina Preda <irinalaurapreda@gmail.com>
2019-03-13 17:15:06 +0000

    fix script to allow generation of smaller datasets from the full MSE data set

--- data-collection/json_analysis/slimAndTrim.py
+++ data-collection/json_analysis/slimAndTrim.py
@@ -1,4 +1,5 @@
 # slim egbot data by saving only { "question": (was body_markdown)..., "answer": (was answers...nested body_markdown)... } from posts that have accepted answers
+# you can also cap to a certain number of data this should take the MSE-full data that you have and slim it down to MSE-100 with just 100 data points)
 
 import json, os
 
@@ -11,13 +12,9 @@ def findFile(prefix):
     for file in os.listdir(buildPath):
         if file.startswith(prefix) and file.endswith(".json"):
             return file
-    return null
+    return ""
 
-# make the output directory
-os.mkdir(buildPath+"slimmer")
-
-count = 0
-for no in range(1,2):
+def trim(no, count):
     # find e.g. MathStackExchangeAPI_Part_1_TimeStamps_1512760268_1535031491.json (e.g. what the Zenodo fils are called)
     filename = findFile("MathStackExchangeAPI_Part_" + str(no))
     filepath = os.path.abspath(buildPath+filename)
@@ -28,8 +25,8 @@ for no in range(1,2):
 
         slim = []
         for i in range(0, len(data)):
-            #if count>=100:
-            #    break
+            if capped and count>=cap:
+                break
             if("answers" in data[i].keys()):
                 for j in range(0, len(data[i]["answers"])):
                     is_accepted = data[i]["answers"][j]["is_accepted"]
@@ -38,12 +35,34 @@ for no in range(1,2):
                         temp["question"] = data[i]["body_markdown"]
                         temp["answer"] = data[i]["answers"][j]["body_markdown"]
                         slim.append(temp)
-                        count += 1                        
-                        
+                        count += 1                                
+        if capped:
+            filename = "MathStackExchangeAPI_" + str(cap)
         outpath = os.path.abspath(buildPath + "slimmer/" + filename)
-        print("count ", count)
+        print("Count: ", count)
         print("Saving ... to ", outpath)
         with open(outpath, 'w') as outfile:  
             json.dump(slim, outfile)
+    return count
+
+## main
+
+# make the output directory
+#os.mkdir(buildPath+"slimmer")
+
+# this can be used if you want to trim to a dataset smaller than one of the 8 files; cap should be less than 50k (otherwise it would have to look at the next file)
+# this will change the name of the file as well to MathStackExchangeAPI_*cap* e.g. MathStackExchangeAPI_500
+capped = True
+cap = 2000 
+# tracker of data points count
+count = 0
+
+if (not capped):
+    for no in range(1,9):
+        count = trim(no, count)
+else:
+    for no in range(1,2):
+        count = trim(no, count)
 
 print("Done :)")
+
