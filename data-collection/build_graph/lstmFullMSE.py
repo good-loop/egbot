352446fbd9f56d7b26c3caab47fdd0f1557c2cbb
Irina Preda <irinalaurapreda@gmail.com>
2019-04-18 16:34:00 +0100

    generating paulius' model

--- data-collection/build_graph/lstmFullMSE.py
+++ data-collection/build_graph/lstmFullMSE.py
@@ -12,6 +12,10 @@ from collections import Counter
 import time
 import json
 import os
+import nltk
+#import spacy
+#nlp = spacy.load('en')
+nltk.download('punkt') # needs a download of nltk lib to use tokeniser
 
 start_time = time.time()
 def elapsed(sec):
@@ -32,15 +36,19 @@ vocabLimit = 100000
 # Training files
 datapath = '/home/irina/winterwell/egbot/data/build/slim/';
 file_list = []
-for i in [1,2,3,4,5,6,7,8]:
-    file_list.append(datapath + 'MathStackExchangeAPI_Part_' + str(i) + '.json')
+for i in [1]:#,2,3,4,5,6,7,8]:
+    #file_list.append(datapath + 'MathStackExchangeAPI_Part_' + str(i) + '.json')
+    #training_file = '/home/irina/winterwell/egbot/data/test_input/belling_the_cat.json'
+    #file_list.append(training_file)
+    training_file = '/home/irina/winterwell/egbot/data/test_input/pauliusSample.json'
+    file_list.append(training_file)
 print("List of files to extract: ", file_list)
 
 # using collections.Counter() because we only want to keep most common words (to prevent memory leaks), limit is set by vocabLimit
 vocabCount = Counter();
 
 def read_data(fname):
-    print("Loading file: " + fname)
+    print("\nLoading file: " + fname)
     with open(fname) as json_file:
         data = json.load(json_file)
     return data
@@ -49,12 +57,17 @@ def processJSONFile(data):
     qa = ""
     for elem in data:
         qa += elem["question"] + " " + elem["answer"] + " "
-    return str(qa.strip())
+    
+    text = str(qa.strip())
+    print("Tokenising...")
+    training_data = nltk.word_tokenize(text)
+    print("Finished tokenising")
+    return training_data
 
 def build_vocab():
     for trainFile in file_list:
         training_data = read_data(trainFile)   
-        words = processJSONFile(training_data).split()
+        words = processJSONFile(training_data)
         count = Counter(words)
         vocabCount.update(count);
 
@@ -67,33 +80,35 @@ print("\nFull vocab size: " + str(vocab_size))
 vocabCount = dict(vocabCount.most_common(vocabLimit))
 vocab_size = len(vocabCount)
 print("Trimmed vocab size: " + str(vocab_size))
+print()
 
 # construct vocab and reverse_vocab as mappings from words to unique id number and vice-versa
 vocab = dict()
 for idx, word in enumerate(vocabCount.keys()):
     vocab[word] = idx
-vocab["<UNKNOWN>"] = idx + 1
+vocab["<UNKNOWN>"] = idx + 1 # only useful if we had to trim the vocab for memory reasons
+vocab["<START>"] = idx + 2
 reverse_vocab = dict(zip(vocab.values(), vocab.keys())) # useful for reverse look-up
 
 # Parameters
-learning_rate = 0.01 
-training_iters = 500
-display_step = 5
-n_input = 3 # sequence length
+learning_rate = 0.0001 
+training_iters = 500000
+display_step = 1000
+n_input = 30 # sequence length
 
 # number of units in RNN cell
-n_hidden = 256
+n_hidden = 512
 
 # tf Graph input
 x = tf.placeholder("float", [None, n_input, 1])
-y = tf.placeholder("float", [None, vocab_size+1])
+y = tf.placeholder("float", [None, vocab_size+2])
 
 # RNN output node weights and biases
 weights = {
-    'out': tf.Variable(tf.random_normal([n_hidden, vocab_size+1]))
+    'out': tf.Variable(tf.random_normal([n_hidden, vocab_size+2]))
 }
 biases = {
-    'out': tf.Variable(tf.random_normal([vocab_size+1]))
+    'out': tf.Variable(tf.random_normal([vocab_size+2]))
 }
 
 def RNN(x, weights, biases):
@@ -115,7 +130,7 @@ def RNN(x, weights, biases):
     return tf.matmul(outputs[-1], weights['out']) + biases['out']
 
 g = tf.get_default_graph() 
-with g.device('/device:CPU:0'):
+with g.device('/device:GPU:0'):
     pred = RNN(x, weights, biases)
 
     # Loss and optimizer
@@ -134,61 +149,64 @@ def dim(a):
         return []
     return [len(a)] + dim(a[0])
 
+def checkIfValidWord(idx, training_data):
+    if training_data[idx] in vocab.keys():
+        return vocab[str(training_data[idx])]
+    else:
+        return vocab["<UNKNOWN>"]
+
 # Launch the graph
 with tf.Session() as session:
     session.run(init)
     step = 0
-    offset = random.randint(0,n_input+1)
-    end_offset = n_input + 1
+    #    pos = random.randint(0,n_input+1)
     acc_total = 0
     loss_total = 0
 
     writer.add_graph(session.graph)
 
-    while step < training_iters:
-        for trainFile in file_list:
-            # Load qa data from file as list of words e.g. ["What", "if", "we", etc.]
-            training_data = processJSONFile(read_data(trainFile)).split()
+    for trainFile in file_list:
+        # Load qa data from file as list of words e.g. ["What", "if", "we", etc.]
+        training_data = processJSONFile(read_data(trainFile))
 
-            # Generate a minibatch. Add some randomness on selection process.
-            if offset > (len(training_data)-end_offset):
-                offset = random.randint(0, n_input+1)
-
-            symbols_in_keys = []
-            idx = 0
-            for i in range(offset, offset+n_input):
-                if training_data[i] in vocab.keys(): # if the word appears in the vocab
-                    symbols_in_keys.append(vocab[ str(training_data[i])])
+        print("Starting training iteration...")
+        while step < training_iters:
+            pos = 0        
+            while pos != len(training_data)-n_input:        
+                symbols_in_keys = []
+                if pos < n_input-1:
+                    for i in range(0,n_input-pos-1):
+                        symbols_in_keys.append(vocab["<START>"])
+                    loc = 0
+                    for idx in range(n_input-pos-1,n_input):
+                        symbols_in_keys.append(checkIfValidWord(idx, training_data))
+                        loc += 1
                 else:
-                    symbols_in_keys.append(vocab["<UNKNOWN>"])
-            symbols_in_keys = np.reshape(np.array(symbols_in_keys), [-1, n_input, 1])
-
-            symbols_out_onehot = np.zeros([vocab_size+1], dtype=float)
-            if training_data[offset+n_input] in vocab.keys():
-                targetLoc = vocab[training_data[offset+n_input]]
-                symbols_out_onehot[targetLoc] = 1.0
-            else:
-                symbols_out_onehot[vocab["<UNKNOWN>"]] = 1.0
-            symbols_out_onehot = np.reshape(symbols_out_onehot,[1,-1])
-            
-            _, acc, loss, onehot_pred = session.run([optimizer, accuracy, cost, pred], \
-                                                    feed_dict={x: symbols_in_keys, y: symbols_out_onehot})
-            loss_total += loss
-            acc_total += acc
-            if (step+1) % display_step == 0:
-                print("Iter= " + str(step+1) + ", Average Loss= " + \
-                    "{:.6f}".format(loss_total/display_step) + ", Average Accuracy= " + \
-                    "{:.2f}%".format(100*acc_total/display_step))
-                print("Elapsed time: ", elapsed(time.time() - start_time))
-                print()
-                acc_total = 0
-                loss_total = 0
-                symbols_in = [training_data[i] for i in range(offset, offset + n_input)]
-                symbols_out = training_data[offset + n_input]
-                symbols_out_pred = reverse_vocab[int(tf.argmax(onehot_pred, 1).eval())]
-                print("%s - [%s] vs [%s]" % (symbols_in,symbols_out,symbols_out_pred))
-            step += 1
-            offset += (n_input+1)
+                    for idx in range(pos-n_input+1,pos+1):
+                        symbols_in_keys.append(checkIfValidWord(idx, training_data))
+                symbols_in_keys = np.reshape(np.array(symbols_in_keys), [-1, n_input, 1])
+                symbols_out_onehot = np.zeros([vocab_size+2], dtype=float)
+                symbols_out_onehot[checkIfValidWord(pos+1, training_data)] = 1.0
+                symbols_out_onehot = np.reshape(symbols_out_onehot,[1,-1])
+                
+                _, acc, loss, onehot_pred = session.run([optimizer, accuracy, cost, pred], \
+                                                        feed_dict={x: symbols_in_keys, y: symbols_out_onehot})
+                loss_total += loss
+                acc_total += acc
+                if (step+1) % display_step == 0:
+                    print("Iter= " + str(step+1) + ", Average Loss= " + \
+                        "{:.6f}".format(loss_total/display_step) + ", Average Accuracy= " + \
+                        "{:.2f}%".format(100*acc_total/display_step))
+                    print("Elapsed time: ", elapsed(time.time() - start_time))
+                    print()
+                    acc_total = 0
+                    loss_total = 0
+                    symbols_in = [training_data[i] for i in range(pos, pos + n_input)]
+                    symbols_out = training_data[pos + n_input]
+                    symbols_out_pred = reverse_vocab[int(tf.argmax(onehot_pred, 1).eval())]
+                    print("%s - [%s] vs [%s]" % (symbols_in,symbols_out,symbols_out_pred))
+                step += 1
+                pos += 1
     print("\nOptimization Finished!")
     print("Elapsed time: ", elapsed(time.time() - start_time))
     print("Run on command line.")
@@ -199,10 +217,16 @@ with tf.Session() as session:
         sentence = input(prompt)
         sentence = sentence.strip()
         words = sentence.split(' ')
-        if len(words) != n_input:
-            continue
+        symbols_in_keys = []
+        if len(words) < n_input:
+            for i in range(0,n_input-1):
+                symbols_in_keys.append(vocab["<START>"])
+        for i in range(len(words)):
+            symbols_in_keys.append(vocab[str(words[i])])
+        if len(words) > n_input:
+            for i in range(len(words)-n_input,len(words)):
+                symbols_in_keys.append(vocab[str(words[i])])
         try:
-            symbols_in_keys = [vocab[str(words[i])] for i in range(len(words))]
             for i in range(32):
                 keys = np.reshape(np.array(symbols_in_keys), [-1, n_input, 1])
                 onehot_pred = session.run(pred, feed_dict={x: keys})
