73999a5d0d802658b1b83c5cde2c75b4c90124bf
Irina Preda <irinalaurapreda@gmail.com>
2019-04-30 12:34:33 +0100

    adding debug data and sorting out activation layers

--- data-collection/build_graph/createLSTMGraphTF.py
+++ data-collection/build_graph/createLSTMGraphTF.py
@@ -9,15 +9,14 @@ from tensorflow.contrib import rnn
 
 # Training Parameters
 learning_rate = 0.001
-#training_steps = 10000
-#batch_size = 128
-#display_step = 200
+#training_steps = 10000 # this is set in java training code instead, specifically EvaluationTest
+#batch_size = 128 # should we batch? it might be tricky to sync this with the java code
 
 # WARNING: this might trip you up, because it needs the vocab size of the training data
 # TODO: this can be solved by having the script run with a parameter that tells it what the vocab size is expected to be (this is useful for the code to be able to run for any training data)
-vocab_size = 1197
+vocab_size = 13346
 num_hidden = 512 # number of units in RNN cell
-seq_length = 3 # length of training window
+seq_length = 30 # length of training window
 
 # Experiment
 experimentName = "lstmGraphTF_vocab=" + str(vocab_size) + "_numHidden=" + str(num_hidden) + "_seqLength=" + str(seq_length) + ".pb";
@@ -50,13 +49,7 @@ def RNN(x, weights, biases):
     words = tf.identity(x, name="words")
 
     # 2-layer LSTM, each layer has num_hidden units.
-    # Average Accuracy= 95.20% at 50k iter
     rnn_cell = rnn.MultiRNNCell([rnn.BasicLSTMCell(num_hidden),rnn.BasicLSTMCell(num_hidden)])
-
-    # 1-layer LSTM with num_hidden units but with lower accuracy.
-    # Average Accuracy= 90.60% 50k iter
-    # Uncomment line below to test but comment out the 2-layer rnn.MultiRNNCell above
-    # rnn_cell = rnn.BasicLSTMCell(num_hidden)
 
     # generate prediction
     outputs, states = rnn.static_rnn(rnn_cell, x, dtype=tf.float32)
@@ -65,19 +58,21 @@ def RNN(x, weights, biases):
     # we only want the last output
     return tf.matmul(outputs[-1], weights['out']) + biases['out']
 
-with g.device('/device:GPU:0'):
-    logits = tf.identity(BiRNN(X, weights, biases), name="logits")
-    #prediction = tf.identity(logits, name='output')    
-    prediction = tf.nn.softmax(logits, name="output")
+g = tf.get_default_graph() 
 
-    # Define loss and optimizer
-    loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=Y), name="loss_op")
+with g.device('/device:GPU:0'):
+    pred = tf.identity(RNN(x, weights, biases), name="logits");
+    relu = tf.nn.relu(pred, name="relu"); # negative values turned to 0 (because negative means deactivated)
+    output = tf.nn.softmax(relu, name="output"); # TODO: should we do sigmoid instead? it's more numerically stable https://groups.google.com/forum/#!topic/theano-dev/Vg08s2UegHU
+
+    # Loss and optimizer
+    loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=output, labels=y), name="loss_op")
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, name="optimizer")
     train_op = optimizer.minimize(loss_op, name="train_op")
 
-# Model evaluation
-correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1), name="correct_pred")
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32), name="accuracy")
+    # Model evaluation
+    correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1), name="correct_pred")
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32), name="accuracy")
 
 # Initializing the variables
 init = tf.global_variables_initializer()
@@ -87,10 +82,14 @@ init = tf.global_variables_initializer()
 saver_def = tf.train.Saver().as_saver_def()
 
 print('Operation to initialize variables:       ', init.name)
-print('Tensor to feed as inp data:            ', x.name)
-print('Tensor to feed as training targets:      ', y.name)
-print('Tensor to fetch as prediction:           ', pred.name)
+print('Tensor to feed as input data:            ', x.name)
+print('Tensor to feed as training target:       ', y.name)
 print('Operation to train one step:             ', train_op.name)
+print('Tensor to fetch raw logits:              ', pred.name)
+print('Tensor to fetch relu outputs:            ', relu.name)
+print('Tensor to fetch prediction outputs:      ', output.name)
+print('Tensor to fetch prediction accuracy:     ', correct_pred.name)
+print('Tensor to fetch training accuracy:       ', accuracy.name)
 print('Tensor to be fed for checkpoint filename:', saver_def.filename_tensor_name)
 print('Operation to save a checkpoint:          ', saver_def.save_tensor_name)
 print('Operation to restore a checkpoint:       ', saver_def.restore_op_name)
