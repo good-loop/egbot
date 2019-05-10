ae1e388f3a979ddca96ee253db62e53af1f1c36b
Irina Preda <irinalaurapreda@gmail.com>
2019-05-10 17:10:31 +0100

    adding nd4j

--- builder/egbot/BuildEgBotJava.java
+++ builder/egbot/BuildEgBotJava.java
@@ -16,8 +16,8 @@ public class BuildEgBotJava extends BuildTask {
 		mdt.addDependency("org.tensorflow", "tensorflow", "1.5.0");
 		mdt.addDependency("org.tensorflow", "libtensorflow", "1.12.0");
 		mdt.addDependency("org.tensorflow", "libtensorflow_jni_gpu", "1.12.0");
-		mdt.addDependency("org.deeplearning4j","deeplearning4j-core","1.0.0-beta2");
-		mdt.addDependency("org.nd4j","nd4j-native-platform","1.0.0-beta2");
+//		mdt.addDependency("org.deeplearning4j","deeplearning4j-core","1.0.0-beta2");
+		mdt.addDependency("org.nd4j","nd4j-native-platform","1.0.0-beta4");
 		mdt.addDependency("org.slf4j","slf4j-log4j12","1.8.0-beta2");
 		mdt.setIncSrc(true);
 		mdt.run();
